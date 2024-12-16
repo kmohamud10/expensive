7f084c9d48bcf0005d67450255165e9fbcfc9486
kmohamud222 <kmohamud222@gmail.com>
2024-12-16 03:21:01 +0000

    first one

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+import csv
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def create_user():
+    if request.method == "POST":
+        Today = request.form["Today"]
+        Task = request.form["Task"]
+        Details = request.form["Details"]
+
+        # Save to CSV
+        with open("file.csv", "a", newline='') as f:
+            writers = csv.writer(f)
+            writers.writerow([Today, Task, Details])
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True) 
